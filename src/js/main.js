7ce3afb0b9807c5804ebb123c82aa1f13c048996
mohseenrm <mohseenmukaddam6@gmail.com>
2017-01-14 11:58:06 -0700

    last build

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,35 @@
 (()=>{
     const name = 'MoMo';
     console.log(`Hello ${name}!`);
+    const client_id = '';
+    const url = `https://api.amazon.com/auth/o2/token`;
+//     $.ajax({
+//         url: 'https://api.amazon.com/auth/o2/',
+//         contentType: 'application/x-www-form-urlencoded',
+//         data: {
+//             format: 'json'
+//         },
+//         error: function() {
+//             $('#info').html('<p>An error has occurred</p>');
+//         },
+//         dataType: 'json',
+//         success: function(data) {
+//             var $title = $('<h1>').text(data.talks[0].talk_title);
+//             var $description = $('<p>').text(data.talks[0].talk_description);
+//             $('#info')
+//                 .append($title)
+//                 .append($description);
+//         },
+//         type: 'POST'
+// });
+    // const url2 = ``;
+    //https://developer.amazon.com/blogs/post/Tx3CX1ETRZZ2NPC/alexa-account-linking-5-steps-to-seamlessly-link-your-alexa-skill-with-login-with-amazon
+    
+    $.post( url, 
+        { 
+            client_id: "John", 
+            time: "2pm" 
+        }, (data)=>{
+            console.log(data);
+        } );
 })();
